8bf219d8a5dd578f1d38191e5937cca68afa4014
marwamsk <msadekmaroua@gmail.com>
2021-12-28 07:03:14 +0100

    creation du serpent 0

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 window.onload = function() {
- var canvas
  var canvasW =900;
  var canvasH =600;
+ var blockSize = 30;
  var ctx; 
- var xCoord = 0;
- var yCoord = 0 ;
+
  var delay = 100; 
+ var snakee ;
  init();
     function init() {
-       var canvas = document.createElement('canvas');
-        canvas.width = canvasW;
-        canvas.height = canvasH;
+        var canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
         canvas.style.border = "1px solid";
         document.body.appendChild (canvas);
         ctx = canvas.getContext('2d');
+        snakee = new Snake ( [[6,4],[5,4],[4,4]])
         refreshCanvas();
         
     }
     function refreshCanvas() {
-        xCoord+=5;
-        yCoord+=5;
+       
         ctx.clearRect(0,0,canvasW,canvasH);
-        ctx.fillStyle="#ff0000";
-        ctx.fillRect(xCoord,yCoord,100,50);
+        snakee.draw();
         setTimeout(refreshCanvas,delay);
+    }
+    function drawBlock(ctx, position){
+        var x = position[0]*blockSize;
+        var y = position[1]*blockSize;
+        ctx.fillRect(x,y,blockSize,blockSize);
+    }
+    function Snake (body){
+        this.body = body ;
+        this.draw = function () {
+            ctx.save();
+            ctx.fillStyle="#ff0000";
+            for(var i=0; i<this.body.length; i++){
+                drawBlock(ctx,this.body[i]); 
+            }
+            ctx.restore();
+           
+
+        };
     }
 
 }
