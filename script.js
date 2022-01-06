618146e0350b9dc4470e4d101bdf0f831af2b3c3
marwamsk <msadekmaroua@gmail.com>
2022-01-06 08:53:23 +0100

    collision mur et body

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ window.onload = function() {
     var blockSize = 30;
     var ctx; 
     var delay = 100; 
-    var snakee ;
-    var applee ;
+    var snakee;
+    var applee;
+    var widthInBlocks = canvasW/blockSize;
+    var heightInBlocks = canvasH/blockSize;
     init();
     function init() {
            var canvas = document.createElement('canvas');
@@ -20,11 +22,17 @@ window.onload = function() {
            
        }
    function refreshCanvas() {
-           ctx.clearRect(0,0,canvasW,canvasH);
-           snakee.advance();
-           snakee.draw();
-           applee.draw();
-           setTimeout(refreshCanvas,delay);
+            snakee.advance();
+            if (snakee.checkCollision() ) {
+                // GAMA OVER
+            }
+            else{
+
+                ctx.clearRect(0,0,canvasW,canvasH);
+                snakee.draw();
+                applee.draw();
+                setTimeout(refreshCanvas,delay);
+            }
        }
    function drawBlock(ctx, position){
            var x = position[0]*blockSize;
@@ -80,6 +88,29 @@ window.onload = function() {
               if(allowdDirections.indexOf(newDirection)> -1){
                   this.direction = newDirection;
               }
+           };
+           this.checkCollision = function(){
+               var wallCollision = false;
+               var snakeCollision = false;
+               var head = this.body[0];
+               var rest = this.body.slice(1);
+               var snakeX = head[0];
+               var snakeY = head[1];
+               var minX = 0;
+               var minY = 0;
+               var maxX = widthInBlocks - 1;
+               var maxY = heightInBlocks - 1;
+               var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
+               var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
+               if(isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls) {
+                   wallCollision = true;
+               }
+               for(var i = 0; i<rest.length;i++) {
+                   if(snakeX === rest[i][0] && snakeY === rest[i][1]) {
+                       snakeCollision = true;
+                   }
+               }
+               return wallCollision || snakeCollision ;
            };
        }
        function Apple(position){
